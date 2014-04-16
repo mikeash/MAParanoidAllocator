2da87d54a01bb4e6d8dba34050b596891fffcc00
Mike Ash <mike@mikeash.com>
2014-04-15 20:13:25 -0400

    Set up tests and ditch prefix headers

---
+++ MAParanoidAllocator Tests/MAParanoidAllocator_Tests.m
@@ -0,0 +1,34 @@
+//
+//  MAParanoidAllocator_Tests.m
+//  MAParanoidAllocator Tests
+//
+//  Created by Michael Ash on 4/15/14.
+//  Copyright (c) 2014 mikeash. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MAParanoidAllocator_Tests : XCTestCase
+
+@end
+
+@implementation MAParanoidAllocator_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
