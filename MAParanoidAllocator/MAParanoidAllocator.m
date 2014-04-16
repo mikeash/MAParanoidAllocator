860f2746c51d7a8f53946f5602b3758fced91f55
Mike Ash <mike@mikeash.com>
2014-04-15 21:23:06 -0400

    Add some basic memory protection code

--- MAParanoidAllocator/MAParanoidAllocator.m
+++ MAParanoidAllocator/MAParanoidAllocator.m
@@ -64,9 +64,24 @@
         }
         
         _memory = afterPointer;
+        _size = size;
+        
+        [self mprotect: PROT_NONE];
+    } else {
+        _size = size;
     }
-    
-    _size = size;
+}
+
+- (void)read: (void (^)(const void *))block {
+    [self withProtections: PROT_READ call: ^{
+        block(_memory);
+    }];
+}
+
+- (void)write: (void (^)(void *))block {
+    [self withProtections: PROT_READ | PROT_WRITE call: ^{
+        block(_memory);
+    }];
 }
 
 - (size_t)roundToPageSize: (size_t)size {
@@ -74,12 +89,17 @@
     return pageCount * _pageSize;
 }
 
-- (void)read: (void (^)(const void *))block {
-    block(_memory);
+- (void)mprotect: (int)prot {
+    size_t size = [self roundToPageSize: _size];
+    if(size > 0) {
+        CHECK(mprotect(_memory, size, prot), < 0);
+    }
 }
 
-- (void)write: (void (^)(void *))block {
-    block(_memory);
+- (void)withProtections: (int)prot call: (void (^)(void))block {
+    [self mprotect: prot];
+    block();
+    [self mprotect: PROT_NONE];
 }
 
 @end
