1f98fd2244731f9e6c3c810de72926152485bab6
Mike Ash <mike@mikeash.com>
2014-04-15 20:06:39 -0400

    Initial Commit

---
+++ MAParanoidAllocator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MAParanoidAllocator
+//
+//  Created by Michael Ash on 4/15/14.
+//  Copyright (c) 2014 mikeash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
